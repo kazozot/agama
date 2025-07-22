b1aa536c7b152de51a000efa801a61225073fc6b
kazozot <92520902+kazozot@users.noreply.github.com>
2025-07-22 14:21:59 +0700

    Add files via upload

--- assets/js/ads/ads-post-start.js
+++ assets/js/ads/ads-post-start.js
@@ -1,12 +1,11 @@
-document.write(`
-<script type="text/javascript">
-	atOptions = {
-		'key' : '89dd05f056df7ba4a61cd242e26cb6af',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/89dd05f056df7ba4a61cd242e26cb6af/invoke.js"></script>
-`);
+atOptions = {
+    'key' : '89dd05f056df7ba4a61cd242e26cb6af',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//www.highperformanceformat.com/89dd05f056df7ba4a61cd242e26cb6af/invoke.js';
+document.currentScript.parentElement.appendChild(script);
